0839783ee02fedea878101aaa516df719b9a5746
Ashutosh singh <55102089+Ashutosh619-sudo@users.noreply.github.com>
2021-10-22 15:17:52 +0530

    add support for timescaleDB
    added support for timescaleDB in mindsDB datasource

---
+++ mindsdb_datasources/datasources/timescale_ds.py
@@ -0,0 +1,12 @@
+import os
+from mindsdb_datasources.datasources.data_source import PostgresDS
+
+
+class TimescaleDS(PostgresDS):
+    def __init__(self,query, database, host,
+                 port, user, password):
+        super.__init__(query, database, host, port, user, password)
+
+    def name(self):
+        return 'Timescale - {}'.format(self._query) 
+
