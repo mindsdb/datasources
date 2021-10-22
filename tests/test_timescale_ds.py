f2967a81c0d181cb1f7c9f8629b2e9eebef9cbe7
Michael Lantz <241893+MichaelLantz@users.noreply.github.com>
2021-10-22 09:43:32 -0500

    Updating TimeScale datasource
    Fixing Typo on Timescale datasource test

--- tests/test_timescale_ds.py
+++ tests/test_timescale_ds.py
@@ -3,7 +3,7 @@ from mindsdb_native import F
 from common import DB_CREDENTIALS, break_dataset
 
 
-class TestPostgres(unittest.TestCase):
+class TestTimeScale(unittest.TestCase):
     def setUp(self):
         self.USER = DB_CREDENTIALS['postgres']['user']
         self.PASSWORD = DB_CREDENTIALS['postgres']['password']
@@ -30,7 +30,7 @@ class TestPostgres(unittest.TestCase):
             )
         )
 
-        timescale.df = break_dataset(postgres_ds.df)
+        timescale.df = break_dataset(timescale_ds.df)
 
         assert len(postgres_ds) == LIMIT
 
