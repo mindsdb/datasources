ad9423f5d72818e1e335422d443632087937f357
ZoranPandovski <zoran.pandovski@gmail.com>
2021-12-02 14:54:05 +0100

    Ignore impala and quest tests

--- tests/test_impala_ds.py
+++ tests/test_impala_ds.py
@@ -1,7 +1,7 @@
 import unittest
 from common import DB_CREDENTIALS, break_dataset
 
-
+@unittest.skip("Ignore untill we have environment ready")
 class TestImpala(unittest.TestCase):
     def setUp(self):
         self.HOST = DB_CREDENTIALS['impala']['host']
